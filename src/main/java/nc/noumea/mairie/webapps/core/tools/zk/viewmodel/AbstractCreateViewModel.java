540821cbe0af314e5ed08ea5a1b904b42fa22f35
sblemand <samuel.blemand@agilesoft.nc>
2018-06-27 16:24:20 +1100

    Ajout d'un évenement au save du createviewmodel

--- src/main/java/nc/noumea/mairie/webapps/core/tools/zk/viewmodel/AbstractCreateViewModel.java
+++ src/main/java/nc/noumea/mairie/webapps/core/tools/zk/viewmodel/AbstractCreateViewModel.java
@@ -10,12 +10,12 @@ package nc.noumea.mairie.webapps.core.tools.zk.viewmodel;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -24,6 +24,8 @@ package nc.noumea.mairie.webapps.core.tools.zk.viewmodel;
 
 import org.zkoss.bind.annotation.*;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zul.Window;
 
@@ -74,6 +76,7 @@ public abstract class AbstractCreateViewModel<T extends AbstractEntity> extends 
 			return;
 		}
 		entity = getService().save(entity);
+		Events.sendEvent(new Event("onEntitySave", this.popup.getParent() == null ? this.popup : this.popup.getParent()));
 		postGlobalCommandRefreshListe();
 		closePopup();
 		if (this.openAfterCreate()) {
